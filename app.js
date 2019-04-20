f27dc115eb0ef113338550894e1d67efe4a2fa44
Jamie Sumner <jamiesumner@Jamies-MacBook-Air.local>
2019-04-20 13:23:34 -0400

    added more HTML

---
+++ app.js
@@ -0,0 +1,8 @@
+// things we need to grab from API: article title; author
+
+var articles = [];
+
+var articleSearch = $("#XXXXX").val().trim();
+
+// The article from the textbox is then added to our array
+articles.push(articleSearch);
